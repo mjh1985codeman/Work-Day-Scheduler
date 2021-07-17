77b6cb2dfae266bf8f8c77903679d61242927d2a
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-07-17 16:13:35 -0500

    created taskTextInput variable as an array that captures text for each click event on the save button

--- script.js
+++ script.js
@@ -8,33 +8,6 @@ displayCurrentDate = function () {
     currentDateTime.format("dddd, MMMM Do YYYY");
 };
 
-// get current hour in standard format.
-//var currentHour = moment().format("LT");
-// get current hour "split" so we can use that to compare w/ the HTML Hour text.
-//var currentHourSplit = currentHour.split(":")[0];
-
-// get hour string array from html class name "hour"
-//var scheduleHourBlocks = document.getElementsByClassName("hour");
-// create a for loop to get the "Hour" from the outerText DOM element
-//so we can compare that with currentHourSplit to create our if/else.
-// for (var i = 0; i < scheduleHourBlocks.length; i++) {
-//   var scheduleHourBlocksSplit = scheduleHourBlocks[i].outerText.split(":")[0];
-//   console.log(scheduleHourBlocksSplit);
-//   if (currentHourSplit === scheduleHourBlocksSplit) {
-//     console.log("This one Matches");
-//     document
-//       .getElementsByClassName("description")[0]
-//       .classList.removeNamedItem("past");
-//     document
-//       .getElementsByClassName("description")[0]
-//       .classList.removeNamedItem("future");
-//     document
-//       .getElementsByClassName("description")[0]
-//       .classList.setNamedItem("present");
-//   } else {
-//     console.log("These do not match");
-//   }
-// }
 // function to color code the rows based on what time it is.
 updateRowColors = function () {
   // using the .each JQuery method to loop through each parent class time-block
@@ -43,25 +16,36 @@ updateRowColors = function () {
     var timeBlockHour = parseInt($(this).attr("data-id"));
     // using the hours method to get the exact hour in military time.
     var presentHour = moment().hours();
-    console.log(timeBlockHour);
-    console.log(presentHour);
-    if (timeBlockHour === presentHour) {
-      $("textarea").addClass("present");
-      console.log("This is the NOW hour");
+
+    if (timeBlockHour == presentHour) {
+      $(this).addClass("present");
     } else if (timeBlockHour < presentHour) {
-      $("textarea").addClass("past");
-      console.log("This is in the past!");
+      $(this).addClass("past");
     } else {
-      $("textarea").addClass("future");
-      console.log("That must mean this is the future");
+      $(this).addClass("future");
     }
   });
 };
-//console.log(currentHour);
-//console.log(scheduleHourBlocks);
-//console.log(currentHourSplit);
 
 //function calls.
 
 displayCurrentDate();
 updateRowColors();
+
+// Trying to figure out how to get all the tasks to be console logged separately.
+$("button").click(function () {
+  console.log("you clicked a button");
+  var taskTextInput = [
+    $("#taskText01").val(),
+    $("#taskText02").val(),
+    $("#taskText03").val(),
+    $("#taskText04").val(),
+    $("#taskText05").val(),
+    $("#taskText06").val(),
+    $("#taskText07").val(),
+    $("#taskText08").val(),
+    $("#taskText09").val(),
+  ];
+
+  console.log(taskTextInput);
+});
