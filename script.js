17c62e83271a8676308f2cc3e0ec67569926c8b6
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-07-16 23:09:34 -0500

    got the hour string array from the HTML class hour, created for loop to get hours via the outerText DOM element and split that to get the 0 index value of that which is the hour listed in the HTML time blocks.

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 // global variables
 
-var currentDate = moment();
+var currentDateTime = moment();
 
-// function to get the currentdatetime
-getCurrentDate = function () {
+// function to get the currrent date and write it to the HTML
+displayCurrentDate = function () {
   document.getElementById("currentDay").innerHTML =
-    currentDate.format("dddd, MMMM Do YYYY");
+    currentDateTime.format("dddd, MMMM Do YYYY");
 };
 
-getCurrentDate();
+displayCurrentDate();
+
+// get current hour in standard format.
+var currentHour = moment().format("LT");
+// get current hour "split" so we can use that to compare w/ the HTML Hour text.
+var currentHourSplit = currentHour.split(":")[0];
+
+// get hour string array from html class name "hour"
+var scheduleHourBlocks = document.getElementsByClassName("hour");
+// create a for loop to get the "Hour" from the outerText DOM element
+//so we can compare that with currentHourSplit to create our if/else.
+for (var i = 0; i < scheduleHourBlocks.length; i++) {
+  var scheduleHourBlocksSplit = scheduleHourBlocks[i].outerText.split(":")[0];
+  console.log(scheduleHourBlocksSplit);
+}
+// function to color code the rows based on what time it is.
+updateRowColors = function () {
+  //console.log(currentHour);
+  //console.log(scheduleHourBlocks);
+  console.log(currentHourSplit);
+};
+
+updateRowColors();
